8d704f3a2e2c2f08b346734abeea4e724f97104e
BIlly Okal <okal.billy@googlemail.com>
2013-04-19 15:25:57 +0200

    initial commit

---
+++ pycss/src/gdevs.py
@@ -0,0 +1,16 @@
+# Gaussian gerivatives using Hermite polynomials
+
+import numpy as np
+
+
+def hermite(deg):
+    """Hermite polynomial of degree deg """
+    return []
+
+
+def gaussian_dev(order, sigma):
+    """Gaussian derivative of a given order """
+    base_gaussian = np.exp(- t**2 / (2*sigma**2))
+
+    return k
+
